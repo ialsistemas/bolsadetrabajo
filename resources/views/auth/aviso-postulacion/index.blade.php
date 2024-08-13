e0fdc5043b4af88e4423f0a7ab3613f57e0a8540
EliasLopezTito <elias.lopez.tito.99@gmail.com>
2024-08-13 16:10:26 -0500

    avisos cambios 3

--- resources/views/auth/aviso-postulacion/index.blade.php
+++ resources/views/auth/aviso-postulacion/index.blade.php
@@ -8,21 +8,23 @@
     <link rel="stylesheet" href="{{ asset('auth/plugins/datatable/datatables.min.css') }}">
 @endsection
 <style type="text/css">
-
-    .txt_claro{
+    .txt_claro {
         background: #79f57f63;
         /* color: #fff; */
     }
+
     .label-as-badge {
         border-radius: 1em;
         font-size: 12px;
         cursor: pointer;
     }
+
     table.dataTable th,
     table.dataTable td {
         white-space: nowrap;
     }
-    .sorting_1{
+
+    .sorting_1 {
         padding-left: 30px !important;
     }
 </style>
@@ -52,7 +54,7 @@
                     <label for="carrera" class="m-0 label-primary">Carrera del Estudiante</label>
                     <select name="carrera" id="carrera" class="form-control-m form-control-sm">
                         <option value="">-- TODOS --</option>
-                        @foreach($Areas as $q)
+                        @foreach ($Areas as $q)
                             <option value="{{ $q->id }}">{{ $q->nombre }}</option>
                         @endforeach
                     </select>
@@ -61,7 +63,7 @@
                     <label for="provincia" class="m-0 label-primary">Provincia del Estudiante</label>
                     <select name="provincia" id="provincia" class="form-control-m form-control-sm">
                         <option value="">-- TODOS --</option>
-                        @foreach($Provincias as $q)
+                        @foreach ($Provincias as $q)
                             <option value="{{ $q->id }}">{{ $q->nombre }}</option>
                         @endforeach
                     </select>
@@ -70,7 +72,7 @@
                     <label for="tipo_estudiante" class="m-0 label-primary">Tipo de Estudiante</label>
                     <select name="tipo_estudiante" id="tipo_estudiante" class="form-control-m form-control-sm">
                         <option value="">-- TODOS --</option>
-                        @foreach($GradoAcademico as $q)
+                        @foreach ($GradoAcademico as $q)
                             <option value="{{ $q->id }}">{{ $q->grado_estado }}</option>
                         @endforeach
                     </select>
@@ -79,10 +81,10 @@
                     <a href="javascript:void(0)" class="btn-m btn-primary-m" onclick="consultarAvisosPostulantes()">
                         <i class="fa fa-search"></i> Consultar</a>
                 </div>
-               {{--  <div class="form-group col-lg-3 col-md-12 d-flex flex-column">
+                {{--  <div class="form-group col-lg-3 col-md-12 d-flex flex-column">
                     <a href="javascript:void(0)" id="btn_mostrar" class="btn-m btn-primary-m" mostrar="" onclick="mostrarTodo()">Mostrar toda la Data</a>
                 </div> --}}
-                
+
             </div>
         </div>
         <hr>
@@ -90,7 +92,6 @@
         <section class="content-header">
             @csrf
             <div class="row">
-                
                 <div class="col-md-12">
                     <div class="row align-items-center">
                         <div class="col-lg-8">
@@ -100,11 +101,13 @@
                             </div>
                         </div>
                         <div class="form-group col-lg-3 col-md-16 d-flex flex-column">
-                            <a href="javascript:void(0)" id="btn_mostrar" class="btn-m btn-primary-m" mostrar="" onclick="mostrarTodo()"
-                            style="padding: 7.5px; background: #464646;"><i class="fa fa-eye"></i> Mostrar toda la Data</a>
+                            <a href="javascript:void(0)" id="btn_mostrar" class="btn-m btn-primary-m" mostrar=""
+                                onclick="mostrarTodo()" style="padding: 7.5px; background: #464646;"><i
+                                    class="fa fa-eye"></i> Mostrar toda la Data</a>
                         </div>
                     </div>
-                    <table id="tableAvisoPostulantes" width="100%" class='display responsive no-wrap table table-bordered table-hover table-condensed'></table>
+                    <table id="tableAvisoPostulantes" width="100%"
+                        class='display responsive no-wrap table table-bordered table-hover table-condensed'></table>
                 </div>
             </div>
             <div class="form-group col-lg-3 col-md-12 d-flex flex-column">
@@ -114,9 +117,6 @@
         </section>
 
     </div>
-
-
-
 @endsection
 
 @section('scripts')
