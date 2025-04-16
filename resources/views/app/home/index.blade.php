a7727fc609a73b152fc8b56dcc89d63f2f890f1a
1UronaldU1 <ronlad_M_Q_2015@outlook.es>
2025-04-16 09:34:26 -0500

    se agrego titulo y icono en el login de bolsa de trabajo

--- resources/views/app/home/index.blade.php
+++ resources/views/app/home/index.blade.php
@@ -1,78 +1,14 @@
-{{-- @extends('app.index') --}}
-
-@section('styles')
-    {{-- <link rel="stylesheet" href="{{ asset('app/css/home/index.css') }}"> --}}
-@endsection
-
-@section('content')
-    {{-- <div id="main">
-        <div class="banner-header">
-            <img src="{{ asset('app/img/bolsa-de-trabajo_azul_oscuro.jpg') }}" class="img-fluid" alt="Bolsa Laboral">
-        </div>
-        <div class="auth-forms">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-6 col-md-6 col-sm-12">
-                        <form action="{{ route('alumno.login.post') }}" method="post" class="assign">
-                            @csrf
-                            <h5>Aplicar a un empleo</h5>
-                            <div class="form-group">
-                                <input type="text" id="usuario_alumno" name="usuario_alumno" class="form-input {{ $errors->has('usuario_alumno') ? ' is-invalid' : '' }}" value="{{ old('usuario_alumno') }}" required>
-                                <label for="usuario_alumno">Usuario</label>
-                                @if ($errors->has('usuario_alumno'))
-                                    <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $errors->first('usuario_alumno') }}</strong>
-                                </span>
-                                @endif
-                            </div>
-                            <div class="form-group">
-                                <input type="password" id="password_alumno" name="password" class="form-input {{ $errors->has('password_alumno') ? ' is-invalid' : '' }}" required>
-                                <label for="password_alumno">Contraseña</label>
-                                @if ($errors->has('password_alumno'))
-                                    <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $errors->first('password_alumno') }}</strong>
-                                </span>
-                                @endif
-                            </div>
-                            <button type="submit">Ingresar</button>
-                            <a href="{{ route('alumno.crear_alumno') }}" class="text-uppercase">Solicitar acceso</a>
-                        </form>
-                    </div>
-
-                    <div class="col-lg-6 col-md-6 col-sm-12">
-                        <form action="{{ route('empresa.login.post') }}" method="post" class="publish">
-                            @csrf
-                            <h5>Publicar una oferta</h5>
-                            <div class="form-group">
-                                <input type="text" id="usuario_empresa" name="usuario_empresa" class="form-input {{ $errors->has('usuario_empresa') ? ' is-invalid' : '' }}" value="{{ old('usuario_empresa') }}" required>
-                                <label for="usuario_empresa">Usuario</label>
-                                @if ($errors->has('usuario_empresa'))
-                                    <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $errors->first('usuario_empresa') }}</strong>
-                                </span>
-                                @endif
-                            </div>
-                            <div class="form-group">
-                                <input type="password" id="password_empresa" name="password" class="form-input {{ $errors->has('password_empresa') ? ' is-invalid' : '' }}" required>
-                                <label for="password_empresa">Contraseña</label>
-                                @if ($errors->has('password_empresa'))
-                                    <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $errors->first('password_empresa') }}</strong>
-                                </span>
-                                @endif
-                            </div>
-                            <button type="submit">Ingresar</button>
-                            <a href="{{ route('empresa.crear_empresa') }}" class="text-uppercase">Crear una cuenta</a>
-                        </form>
-                    </div>
-
-                </div>
-            </div>
-        </div>
-        <div></div>
-    </div> --}}
-@endsection
-<script src="https://kit.fontawesome.com/6f8129a9b1.js" crossorigin="anonymous"></script>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bolsa de Trabajo</title>
+    <link rel="shortcut icon" href="{{ asset('app/img/logo_ial.png') }}" type="image/x-icon">
+</head>
+<body>
+    <script src="https://kit.fontawesome.com/6f8129a9b1.js" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="{{ asset('app/css/home/login.css') }}">
 <section class="section_login">
     <div class="content_view_login"
@@ -170,3 +106,5 @@
         }
     }
 </script>
+</body>
+</html>
