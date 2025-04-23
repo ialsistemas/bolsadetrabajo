95f7c5f0041f86642dc34ed7fb01608f9b927742
1UronaldU1 <ronlad_M_Q_2015@outlook.es>
2025-04-23 10:53:27 -0500

    se modifico y se agrego cosas para la programación de empleabilidad desde administrador y alumno v27

--- app/Http/Controllers/App/AvisoController.php
+++ app/Http/Controllers/App/AvisoController.php
@@ -359,17 +359,17 @@ class AvisoController extends Controller
         $entity = ParticipantesEmpleabilidad::find($idDesencriptado);
         $programas = [
             "DESPEGA 360" => [
-                "template" => public_path('app/img/template/template-despega-360.jpg'),
+                "template" => "app/img/template/template-despega-360.jpg",
                 "font-size" => "31px",
                 "id" => 1
             ],
             "SKILLS TO WORK" => [
-                "template" => public_path('app/img/template/template-skills-to-work.jpg'),
+                "template" => "app/img/template/template-skills-to-work.jpg",
                 "font-size" => "31px",
                 "id" => 2
             ],
             "CARRERA PRO" => [
-                "template" => public_path('app/img/template/template-carrra-pro.jpg'),
+                "template" => "app/img/template/template-carrra-pro.jpg",
                 "font-size" => "31px",
                 "id" => 3
             ]
